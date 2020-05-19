3d54b1b42cb92ee22b172ca091bf292fb7997b53
Vincent <vincethewalker@yahoo.co.uk>
2020-05-19 18:28:18 +0100

    Added Name Field to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_120534) do
+ActiveRecord::Schema.define(version: 2020_05_19_171418) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_120534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "profile_id"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
