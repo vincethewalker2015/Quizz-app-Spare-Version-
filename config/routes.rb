b694d1996511fc0587fcf143d2730ad132cf23b5
Vincent <vincethewalker@yahoo.co.uk>
2020-05-20 11:53:33 +0100

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   get 'profile', to: 'pages#profile'
+
+  resources :students, :only => [:index]
+  resources :teachers, :only => [:index]
+
+  resources :exams  
+  resources :student_answers, :only => [:create]
 end
